d430b84399fc658e233ebb70db3f8bbed1047c23
Gustavo Luis Silva de Siqueira <gustavoluissilvasiqueira@gmail.com>
2020-03-31 09:43:44 -0300

    Adicionando arquvios da seção 3 - Fundamentos de Javascript da aula 9 a 22.

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,14 @@
+//Comentario de uma linha
+console.log('Linha 1')
+
+/*Comentários 
+de múltiplas LINHAS
+*/ 
+console.log('Linha 2');
+
+/*
+*Comentário de 
+*múltiplas linhas 
+*
+*/
+console.log('Linha 3')
---
+++ fundamentos/organização.js
@@ -0,0 +1,17 @@
+//Organizado em sentenças
+console.log("Sentença do código");
+/*------------------ Organziando 2-------------*/
+//Organizado em blocos 
+{
+    //O seu papel é agrupar sentenças de códigos 
+    {
+        //Bloco 2
+        console.log("Olá");
+        {
+            //Bloco 3
+            console.log('Mundo');
+        }
+    }
+}
+
+
